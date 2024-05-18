7e99c5feb2b0618307fef4b99f504f2d57d57705
hsxiaobai <hsxb2024@163.com>
2024-05-18 21:54:11 +0800

    Update Brainfuck.py

--- Brainfuck.py
+++ Brainfuck.py
@@ -1,51 +1,79 @@
-
 class BrainfuckAnalysisedError(ValueError):
-    pass
+	pass
 
 class BrainfuckCompiledError(ValueError):
-    pass
+	pass
+
+class BrainfuckNotAnalysised(ValueError):
+	pass
 
 class Brainfuck:
-    def __init__(self, code, memorySize=1024):
-        self.code = code
-        self.memorySize = memorySize
-        self.memory = [0 for i in range(self.memorySize)]
-        self.ptr = 0
-        self.state = 0
+	def __init__(self, code, memorySize=1024):
+		self.code = code
+		self.memorySize = memorySize
+		self.memory = [0 for i in range(self.memorySize)]
+		self.ptr = 0
+		self.state = 0
 
-    def grammarAnalysis(self):
-        if self.state != 0:
-            if self.state == 1:
-                raise BrainfuckAnalysisedError("Your Brainfuck module has already been analyzed.")
-            elif self.state == 2:
-                raise BrainfuckCompiledError("Your Brainfuck module has already been compiled.")
-        codeList = list(self.code)
-        result = []
-        charTypeDict = {
-            ",": "io",
-            ".": "io",
-            "+": "op",
-            "-": "op",
-            "<": "ptr",
-            ">": "ptr",
-        }
-        depth = 0
-        block_start = {}
-        for ind, char in enumerate(codeList):
-            if char in charTypeDict:
-                result.append({"type": charTypeDict[char], "command": char})
-            elif char == "[":
-                depth += 1
-                block_start[depth] = ind  # 记录每个深度的起始位置
-            elif char == "]":
-                if depth == 0:
-                    raise ValueError("Unmatched ']' found.")
-                start_ind = block_start[depth]
-                block_code = "".join(codeList[start_ind+1:ind])
-                newBrainfuck = Brainfuck(block_code)
-                newBrainfuck.grammarAnalysis()
-                nested_analysis = newBrainfuck.code
-                result.append({"type": "block", "command": nested_analysis})
-                depth -= 1
-        self.code = result
-        self.state = 1
+	def grammarAnalysis(self):
+		if self.state != 0:
+			if self.state == 1:
+				raise BrainfuckAnalysisedError("Your Brainfuck module has already been analyzed.")
+			elif self.state == 2:
+				raise BrainfuckCompiledError("Your Brainfuck module has already been compiled.")
+		codeList = list(self.code)
+		result = []
+		charTypeDict = {
+			",": "io",
+			".": "io",
+			"+": "op",
+			"-": "op",
+			"<": "ptr",
+			">": "ptr"
+		}
+		depth = 0
+		blockStart = {}
+		for ind, char in enumerate(codeList):
+			if char in charTypeDict:
+				result.append({"type": charTypeDict[char], "command": char})
+			elif char == "[":
+				depth += 1
+				blockStart[depth] = ind
+			elif char == "]":
+				if depth == 0:
+					raise ValueError("Unmatched ']' found.")
+				startInd = blockStart[depth]
+				blockCode = "".join(codeList[startInd+1:ind])
+				newBrainfuck = Brainfuck(blockCode)
+				newBrainfuck.grammarAnalysis()
+				nestedAnalysis = newBrainfuck.code
+				result.append({"type": "block", "command": nestedAnalysis})
+				depth -= 1
+		self.code = result
+		self.state = 1
+	def compileToPython(self):
+		if state != 1:
+			if state == 0:
+				raise BrainfuckNotAnalysisedError("Your Brainfuck module hasn't been analysised yet.")
+			elif state == 2:
+				raise BrainfuckCompiledError("Your Brainfuck module has already been compiled.")
+		result = ""
+		charDict = {
+			",":"self.memory[self.ptr] = ord(input()[1])",
+			".":"print(chr(self.memory[self.ptr])",
+			"+":"self.memory[self.ptr] += 1",
+			"-":"self.memory[self.ptr] -= 1",
+			"<":"""if self.ptr == 0:
+				self.ptr = self.memorySize - 1
+			else:
+   				self.ptr -= 1""",
+			">":"""if self.ptr == self.memorySize - 1:
+   				self.ptr = 0
+	   		else:
+	  			self.ptr += 1"""
+		}
+		for command in code:
+			if command["type"] in ("io","op","ptr"):
+				result += charDict[command["command"]] + "\n"
+			elif command["type"] == "block":
+				pass
